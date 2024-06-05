1e3c3959500d500d3c4e8ae7245fbfba901f95c8
Mohammed Zaheer <152991044+MohammedZaheer0@users.noreply.github.com>
2024-06-05 11:33:54 +0530

    commited

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -28,11 +28,11 @@ const Testimonials = () => {
             <h3 className='text-center'>Testimonials</h3>
             <h1 className='text-center py-[10px] font-bold text-2xl'>What People Say About Us</h1>
 
-            <div className='text-center testimonailsbgimg opacity-85  w-[70%] my-[20px] mx-auto'>
+            <div className='text-center testimonailsbgimg opacity-85  w-[80%] my-[20px] mx-auto'>
 
-            <div className='w-[60%] mx-auto text-center flex flex-col justify-center items-center my-[10px]'>
-                <img src="TestImg.png" alt="testimg" className='testhw my-[20px]' />
-                <h1 className='font-bold lg:text-3xl text-2xl my-[30px]'>"In an emergency situation, I was amazed by how quickly they accommodated me. The dentist's skill and compassion were evident. I'm grateful for their prompt care."</h1>
+            <div className='lg:w-[60%] sm:w-[70%] w-[90%] mx-auto text-center flex flex-col justify-center items-center my-[10px]'>
+                <img src="TestImg.png" alt="testimg" className='testhw sm:my-[20px] my-[10px]' />
+                <h1 className='font-bold lg:text-3xl text-1xl sm:text-2xl my-[30px]'>"In an emergency situation, I was amazed by how quickly they accommodated me. The dentist's skill and compassion were evident. I'm grateful for their prompt care."</h1>
 
                 <h3 className='font-bold mt-[15px]'>David Gahan</h3>
                 <h3>Detroit, Michigan</h3>
@@ -49,11 +49,11 @@ const Testimonials = () => {
     <h3 className='text-center'>Testimonials</h3>
     <h1 className='text-center py-[10px] font-bold text-2xl'>What People Say About Us</h1>
 
-    <div className='text-center testimonailsbgimg opacity-85  w-[70%] my-[20px] mx-auto'>
+    <div className='text-center testimonailsbgimg opacity-85  w-[80%] my-[20px] mx-auto'>
 
-    <div className='w-[60%] mx-auto text-center flex flex-col justify-center items-center my-[10px]'>
-        <img src="TestImg.png" alt="testimg" className='testhw my-[20px]' />
-        <h1 className='font-bold lg:text-3xl text-2xl my-[30px]'>"In an emergency situation, I was amazed by how quickly they accommodated me. The dentist's skill and compassion were evident. I'm grateful for their prompt care."</h1>
+    <div className='lg:w-[60%] sm:w-[70%] w-[90%] mx-auto text-center flex flex-col justify-center items-center my-[10px]'>
+        <img src="TestImg.png" alt="testimg" className='testhw sm:my-[20px] my-[10px]' />
+        <h1 className='font-bold lg:text-3xl text-1xl sm:text-2xl my-[30px]'>"In an emergency situation, I was amazed by how quickly they accommodated me. The dentist's skill and compassion were evident. I'm grateful for their prompt care."</h1>
 
         <h3 className='font-bold mt-[15px]'>David Gahan</h3>
         <h3>Detroit, Michigan</h3>
